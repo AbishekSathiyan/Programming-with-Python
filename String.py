bdf735f858c52ed44d1d92289aa0b1f91ae786e7
AbishekSathiyan <abidinesh2213.gmail.com>
2024-11-20 20:23:37 +0530

    String

--- String.py
+++ String.py
@@ -1,6 +1,12 @@
 name="Abi"
-message=" hello world"
-
+message="hello world"
+print(len(message))
+print(message.find("a"))
+print(message.count("l"))
+print(message*10)
+print(message.replace("l","o"))
+print(message.isalpha)
+print(message.isdigit)
 print(message,name)
 
 quote='"hello world"'
@@ -12,3 +18,13 @@ print(name.upper()) # (.) Invoking Method
 #print(name.lower)
 
 print(message.title())
+
+
+#String Concatination
+
+name="Sandy"
+wish="Happy Birthday"
+
+print(name+"\t"+wish)
+
+print(len(wish))
