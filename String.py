63d17a02d89e3d95e325e00ce3f650918b40280d
AbishekSathiyan <abidinesh2213.gmail.com>
2024-11-20 19:58:48 +0530

    String

---
+++ String.py
@@ -0,0 +1,14 @@
+name="Abi"
+message=" hello world"
+
+print(message,name)
+
+quote='"hello world"'
+
+print(quote)
+
+print(name.upper()) # (.) Invoking Method
+
+#print(name.lower)
+
+print(message.title())
