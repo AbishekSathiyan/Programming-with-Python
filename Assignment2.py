82bcc7b29a3b97134471321094b4703ff0b08208
AbishekSathiyan <abidinesh2213.gmail.com>
2024-11-26 11:15:53 +0530

    Assignment2.py

---
+++ Assignment2.py
@@ -0,0 +1,12 @@
+#Assignment User Input
+
+name=input("Enter Your Name: ")
+mail=input("Enter Your Email: ")
+phone=int(input("Enter Your Mobile Number: "))
+
+print("UserName: ",name)
+
+print("Mail: ",mail)
+
+print("Phone: ",phone)
+
